e368aa20004190aeefc6937a6b27c891316ed78e
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-09-23 18:54:15 -0500

    Add json parsing
    Squashed commit of the following:
    
    commit 1c618df78e63e34c58ee691387957a142cc4d465
    Author: Benjamin Toll <themagicrabbit3of6@gmail.com>
    Date:   Tue Sep 23 18:22:45 2025 -0500
    
        Remove profanity from chirps
    
    commit 523ef15112d7a65f83ab4d967d16d5794df45ba5
    Author: Benjamin Toll <themagicrabbit3of6@gmail.com>
    Date:   Tue Sep 23 16:18:06 2025 -0500
    
        Validate length of chirp

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"slices"
+	"strings"
 	"sync/atomic"
 )
 
@@ -10,11 +13,78 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
+type validateChirpBody struct {
+	Body string `json:"body"`
+}
+
+var Profanity = []string{
+	"kerfuffle",
+	"sharbert",
+	"fornax",
+}
+
 
 func handleHealthz(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Add("content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK\n"))
+}
+
+func handleValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type validateResponse struct {
+		Error string `json:"error"`
+		Valid bool   `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
+	}
+	
+	var responseCode int
+	responseBody := validateResponse{}
+
+	bodyDecoder := json.NewDecoder(r.Body)
+	chirp := validateChirpBody{}
+	if err := bodyDecoder.Decode(&chirp); err != nil {
+		responseBody.Error = "Something went wrong"
+		responseCode = 400
+	} else {
+		if len(chirp.Body) > 140 {
+			responseBody.Error = "Chirp is too long"
+			responseCode = 400
+		} else {
+			message, _ := profanityChecker(chirp.Body)
+			responseBody.CleanedBody = message
+			responseBody.Valid = true
+			responseCode = 200
+		}
+	}
+	data, err := json.Marshal(responseBody)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(responseCode)
+	w.Write(data)
+}
+
+// Returns message string and had_profanity boolean. had_profanity is true if any replacements were
+// performed and false otherwise.
+func profanityChecker(message string) (string, bool) {
+	profanityReplacement := "****"
+	words := strings.Fields(message)
+	hadProfanity := false
+	cleanWords := []string{}
+	for _, w := range words {
+		if slices.Contains(Profanity, strings.ToLower(w)) {
+			cleanWords = append(cleanWords, profanityReplacement)
+			hadProfanity = true
+		} else {
+			cleanWords = append(cleanWords, w)
+		}
+	}
+	if hadProfanity {
+		return strings.Join(cleanWords, " "), hadProfanity
+	}
+	return message, hadProfanity
 }
 
 
@@ -56,6 +126,7 @@ func main() {
 		Addr: ":8080",
 	}
 	mux.HandleFunc("GET /api/healthz", handleHealthz)
+	mux.HandleFunc("POST /api/validate_chirp", handleValidateChirp)
 	mux.HandleFunc("GET /admin/metrics", cfg.handleAppHits)
 	mux.HandleFunc("POST /admin/reset", cfg.resetHitCounter)
 	server.ListenAndServe()
